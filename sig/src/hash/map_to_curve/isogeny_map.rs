d774c86f81aecda8bcd2d8d6cd5b35a6a2fbb927
yuxqiu <yuxqiu@proton.me>
2025-02-03 13:03:36 +0000

    feat: add tests for isogeny map

--- sig/src/hash/map_to_curve/isogeny_map.rs
+++ sig/src/hash/map_to_curve/isogeny_map.rs
@@ -72,6 +72,8 @@ where
         );
 
         // batch_inversion(&mut v);
+
+        // inverse will give 0 by default when `domain_point.x == zero`
         let v: [FpDomainCoDomain; 2] = [
             x_den.evaluate(&domain_point.x)?.inverse()?,
             y_den.evaluate(&domain_point.x)?.inverse()?,
@@ -99,4 +101,95 @@ where
 
         Ok(AffineVar::new(x, y, domain_point.infinity))
     }
+}
+
+#[cfg(test)]
+mod test {
+    use ark_bls12_381::{Fq, Fq2Config};
+    use ark_ec::short_weierstrass::Affine;
+    use ark_r1cs_std::alloc::AllocVar;
+    use ark_r1cs_std::fields::fp::FpVar;
+    use ark_r1cs_std::R1CSVar;
+    use ark_r1cs_std::{
+        fields::{fp2::Fp2Var, FieldVar},
+        groups::curves::short_weierstrass::AffineVar,
+        prelude::Boolean,
+    };
+    use ark_relations::r1cs::ConstraintSystem;
+
+    use crate::hash::map_to_curve::isogeny_map::{Domain, IsogenyMapGadget};
+
+    macro_rules! generate_isogeny_map_tests {
+        ($test_name:ident, $domain_config:ty, $field_var:ty, $base_field:ty) => {
+            #[test]
+            fn $test_name() {
+                fn test_constant() {
+                    {
+                        // Test with identity point
+                        let id = Affine::<Domain<$domain_config>>::identity();
+                        let id_var: AffineVar<Domain<$domain_config>, $field_var, $base_field> =
+                            AffineVar::new(
+                                <$field_var>::constant(id.x),
+                                <$field_var>::constant(id.y),
+                                Boolean::constant(id.infinity),
+                            );
+                        let mapped_id_var =
+                            IsogenyMapGadget::<$domain_config, $field_var, $base_field>::apply(
+                                id_var,
+                            );
+
+                        assert!(mapped_id_var.is_ok());
+
+                        let mapped_id_var = mapped_id_var.unwrap();
+
+                        assert!(mapped_id_var.x.is_constant());
+                        assert!(mapped_id_var.y.is_constant());
+                        assert!(mapped_id_var.infinity.is_constant());
+                        assert!(mapped_id_var.infinity.value().unwrap());
+                    }
+                }
+
+                fn test_input() {
+                    {
+                        let cs = ConstraintSystem::new_ref();
+
+                        // Test with identity point
+                        let id = Affine::<Domain<$domain_config>>::identity();
+                        let id_var: AffineVar<Domain<$domain_config>, $field_var, $base_field> =
+                            AffineVar::new(
+                                <$field_var>::new_input(cs.clone(), || Ok(id.x)).unwrap(),
+                                <$field_var>::new_input(cs.clone(), || Ok(id.y)).unwrap(),
+                                Boolean::new_input(cs.clone(), || Ok(id.infinity)).unwrap(),
+                            );
+                        let mapped_id_var =
+                            IsogenyMapGadget::<$domain_config, $field_var, $base_field>::apply(
+                                id_var,
+                            );
+
+                        assert!(mapped_id_var.is_ok());
+
+                        let mapped_id_var = mapped_id_var.unwrap();
+                        assert!(mapped_id_var.infinity.value().unwrap());
+                    }
+                }
+
+                test_constant();
+                test_input();
+            }
+        };
+    }
+
+    generate_isogeny_map_tests!(
+        test_isogeny_map_g1,
+        ark_bls12_381::g1::Config,
+        FpVar<Fq>,
+        Fq
+    );
+
+    generate_isogeny_map_tests!(
+        test_isogeny_map_g2,
+        ark_bls12_381::g2::Config,
+        Fp2Var<Fq2Config>,
+        Fq
+    );
 }
