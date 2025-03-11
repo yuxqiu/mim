7545808096c393b1343e6c6fa5b4005efd62f270
yuxqiu <yuxqiu@proton.me>
2025-03-11 11:33:27 +0000

    test: reorganize tests

--- sig/src/lib.rs
+++ sig/src/lib.rs
@@ -51,60 +51,6 @@ mod tests {
         (msg, params, sk, pk, sig)
     }
 
-    fn get_aggregate_instances() -> (
-        &'static str,
-        Parameters,
-        Vec<SecretKey>,
-        Vec<PublicKey>,
-        Signature,
-    ) {
-        const N: usize = 1000;
-
-        let msg = "Hello World";
-        let mut rng = thread_rng();
-
-        let params = Parameters::setup();
-        let secret_keys: Vec<SecretKey> = (0..N).map(|_| SecretKey::new(&mut rng)).collect();
-        let public_keys: Vec<PublicKey> = secret_keys
-            .iter()
-            .map(|sk| PublicKey::new(sk, &params))
-            .collect();
-
-        let sig = Signature::aggregate_sign(msg.as_bytes(), &secret_keys, &params).unwrap();
-
-        (msg, params, secret_keys, public_keys, sig)
-    }
-
-    #[test]
-    fn check_signature() {
-        let (msg, params, _, pk, sig) = get_instance();
-        assert!(Signature::verify_slow(msg.as_bytes(), &sig, &pk, &params));
-        assert!(Signature::verify(msg.as_bytes(), &sig, &pk, &params));
-    }
-
-    #[test]
-    fn check_verify_failure() {
-        let (msg, params, _, pk, sig) = get_instance();
-        assert!(!Signature::verify_slow(
-            &[msg.as_bytes(), &[1]].concat(),
-            &sig,
-            &pk,
-            &params
-        ));
-        assert!(!Signature::verify(
-            &[msg.as_bytes(), &[1]].concat(),
-            &sig,
-            &pk,
-            &params
-        ));
-    }
-
-    #[test]
-    fn check_aggregate_signature() {
-        let (msg, params, _, public_keys, sig) = get_aggregate_instances();
-        assert!(Signature::aggregate_verify(msg.as_bytes(), &sig, &public_keys, &params).unwrap());
-    }
-
     #[test]
     fn check_r1cs() {
         let cs = ConstraintSystem::new_ref();
