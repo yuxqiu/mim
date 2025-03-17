0efca6bf26852a7e927c51803d3a8b29e9eb9f46
yuxqiu <yuxqiu@proton.me>
2025-03-17 12:34:34 +0000

    exp: separate native and emulation

--- sig/benches/README.md
+++ sig/benches/README.md
@@ -8,18 +8,22 @@ This file benchmarks two ways to verify BLS signature.
 - One is by directly comparing the results of two pairings
 - The other is by running Miller's loop on both side and performing only one final exponentiation
 
-## `groth16_single_step`
+## `groth16_single_step_native` and `groth16_single_step_emulation`
 
-This file measures the time to generate public parameters and proofs and verify BLS signatures using Groth16. The feature flags allow you to control which curve is used for BLS signatures and SNARK
+These file measures the time to generate public parameters and proofs and verify BLS signatures using Groth16.
+
+The feature flags allow you to control which curve is used for BLS signatures and SNARK
 - `sig-12381-snark-12377`: BLS signature runs on BLS12-381 and SNARK runs on BLS12-377. This setup uses field emulation.
 - `sig-12377-snark-761`: BLS signature runs on BLS12-377, SNARK runs on BW6-761. This setup uses native field variables as the curves are two-chain curves.
+
+As field emulation takes a long time, `groth16_single_step_emulation` is created separately to not use `Criterion` (which requires a sample size of at least 10) for benchmarking. Instead, it uses Rust's built-in `Duration` to measure the wall clock running time. It's recommended to use `groth16_single_step_emulation` when measuring time on `sig-12381-snark-12377`.
 
 For example,
 
 ```sh
 # benchmark using BLS12-381 and BLS12-377
-cargo bench --bench groth16_single_step --no-default-features --features sig-12381-snark-12377
+cargo bench --bench groth16_single_step_emulation --no-default-features --features sig-12381-snark-12377
 
 # benchmark using BLS12-377 and BW6-761
-cargo bench --bench groth16_single_step --no-default-features --features sig-12377-snark-761
+cargo bench --bench groth16_single_step_native --no-default-features --features sig-12377-snark-761
 ```
---
+++ sig/benches/groth16_single_step_emulation.rs
@@ -0,0 +1,103 @@
+use ark_groth16::Groth16;
+use ark_snark::{CircuitSpecificSetupSNARK, SNARK};
+use rand::thread_rng;
+use sig::bls::{BLSCircuit, Parameters, PublicKey, SNARKCurve, SecretKey, Signature};
+
+#[macro_export]
+macro_rules! timeit {
+    ($label:expr, $block:block) => {{
+        use std::time::Instant;
+        let start = Instant::now();
+        let result = $block;
+        let duration = start.elapsed();
+        println!("Time elapsed in {}: {:?}", $label, duration);
+        result
+    }};
+}
+
+fn get_instance() -> (&'static str, Parameters, SecretKey, PublicKey, Signature) {
+    let msg = "Hello World";
+    let mut rng = thread_rng();
+
+    let params = Parameters::setup();
+    let sk = SecretKey::new(&mut rng);
+    let pk = PublicKey::new(&sk, &params);
+
+    let sig = Signature::sign(msg.as_bytes(), &sk, &params);
+
+    (msg, params, sk, pk, sig)
+}
+
+fn bench_groth16() {
+    let (msg, params, _, pk_bls, sig) = get_instance();
+    let mut rng = thread_rng();
+
+    // ===============Setup pk and vk===============
+    let mut pk_vk_gen = || {
+        // in setup node, we don't need to provide assignment
+        let msg = vec![None; msg.len()];
+        let circuit = BLSCircuit::new(None, None, &msg, None);
+        Groth16::<SNARKCurve>::setup(circuit.clone(), &mut rng).unwrap()
+    };
+
+    {
+        timeit!("pk and vk generation", {
+            pk_vk_gen();
+        });
+    }
+
+    let (pk, vk) = pk_vk_gen();
+
+    let pvk_gen = || Groth16::<SNARKCurve>::process_vk(&vk).unwrap();
+
+    {
+        timeit!("pvk generation", {
+            pvk_gen();
+        });
+    }
+
+    let pvk = Groth16::<SNARKCurve>::process_vk(&vk).unwrap();
+
+    // ===============Setup circuit===============
+    let msg = msg
+        .as_bytes()
+        .iter()
+        .copied()
+        .map(Option::Some)
+        .collect::<Vec<_>>();
+
+    let circuit = BLSCircuit::new(Some(params), Some(pk_bls), &msg, Some(sig));
+
+    // ===============Get public inputs===============
+    let public_inputs = circuit.get_public_inputs().unwrap();
+
+    // ===============Create a proof===============
+    let proof_gen =
+        || Groth16::<SNARKCurve>::create_proof_with_reduction_no_zk(circuit.clone(), &pk).unwrap();
+
+    {
+        timeit!("proof generation", {
+            proof_gen();
+        });
+    }
+
+    let proof = proof_gen();
+
+    // ===============Verify the proof===============
+    let verification =
+        || Groth16::<SNARKCurve>::verify_with_processed_vk(&pvk, &public_inputs, &proof).unwrap();
+
+    {
+        timeit!("verification", {
+            verification();
+        });
+    }
+
+    let verified = verification();
+    assert!(verified);
+    println!("Proof verified successfully!");
+}
+
+fn main() {
+    bench_groth16();
+}
