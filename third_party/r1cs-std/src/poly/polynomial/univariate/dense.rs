125df5489412c8fcc6b34d01631946fe344104f3
yuxqiu <yuxqiu@proton.me>
2025-03-18 17:13:37 +0000

    fix: rename `DensePolyVar` and port sonobe's modification to r1cs-std

--- third_party/r1cs-std/src/poly/polynomial/univariate/dense.rs
+++ third_party/r1cs-std/src/poly/polynomial/univariate/dense.rs
@@ -3,18 +3,53 @@ use core::marker::PhantomData;
 use ark_ff::{Field, PrimeField};
 use ark_relations::r1cs::SynthesisError;
 
-use crate::fields::FieldVar;
+use crate::fields::{fp::FpVar, FieldVar};
 use ark_std::vec::Vec;
 
 /// Stores a polynomial in coefficient form, where coeffcient is represented by
+/// a list of `Fpvar<F>`.
+pub struct DensePolynomialVar<F: PrimeField> {
+    /// The coefficient of `x^i` is stored at location `i` in `self.coeffs`.
+    pub coeffs: Vec<FpVar<F>>,
+}
+
+impl<F: PrimeField> DensePolynomialVar<F> {
+    /// Constructs a new polynomial from a list of coefficients.
+    pub fn from_coefficients_slice(coeffs: &[FpVar<F>]) -> Self {
+        Self::from_coefficients_vec(coeffs.to_vec())
+    }
+
+    /// Constructs a new polynomial from a list of coefficients.
+    pub fn from_coefficients_vec(coeffs: Vec<FpVar<F>>) -> Self {
+        Self { coeffs }
+    }
+
+    /// Evaluates `self` at the given `point` and just gives you the gadget for
+    /// the result. Caution for use in holographic lincheck: The output has
+    /// 2 entries in one matrix
+    pub fn evaluate(&self, point: &FpVar<F>) -> Result<FpVar<F>, SynthesisError> {
+        let mut result: FpVar<F> = FpVar::zero();
+        // current power of point
+        let mut curr_pow_x: FpVar<F> = FpVar::one();
+        for i in 0..self.coeffs.len() {
+            let term = &curr_pow_x * &self.coeffs[i];
+            result += &term;
+            curr_pow_x *= point;
+        }
+
+        Ok(result)
+    }
+}
+
+/// Stores a polynomial in coefficient form, where coeffcient is represented by
 /// a list of `FieldVar<F: Field, CF: PrimeField>`.
-pub struct DensePolynomialVar<FP: FieldVar<F, CF>, F: Field, CF: PrimeField> {
+pub struct DensePolynomialVarGeneric<FP: FieldVar<F, CF>, F: Field, CF: PrimeField> {
     /// The coefficient of `x^i` is stored at location `i` in `self.coeffs`.
     pub coeffs: Vec<FP>,
     _params: PhantomData<(F, CF)>,
 }
 
-impl<FP: FieldVar<F, CF>, F: Field, CF: PrimeField> DensePolynomialVar<FP, F, CF> {
+impl<FP: FieldVar<F, CF>, F: Field, CF: PrimeField> DensePolynomialVarGeneric<FP, F, CF> {
     /// Constructs a new polynomial from a list of coefficients.
     pub fn from_coefficients_slice(coeffs: &[FP]) -> Self {
         Self::from_coefficients_vec(coeffs.to_vec())
